4ff18dc6829f87179ab3ee32de2a044530e05e05
Yuha Lee <yuha.lee.dev@gmail.com>
2021-10-25 22:24:39 +0900

    Create App component

---
+++ src/client/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const posts = [
+    {
+        id: 1,
+        text: "Lorem ipsum",
+        user: {
+            avatar: "/uploads/avatar1.jpg",
+            username: "Test User 1"
+        }
+    },
+    {
+        id: 2,
+        text: "Lorem ipsum",
+        user: {
+            avatar: "/uploads/avatar2.jpg",
+            username: "Test User 2"
+        }
+    },
+];
+
+export default function App() {
+    return (
+        <div className="container">
+            <div className="feed">
+                {posts.map(post => 
+                    <div key={post.id} className="post">
+                        <div className="header">
+                            <img src={post.user.avatar} />
+                            <h2>{post.user.username}</h2>
+                        </div>
+                        <p className="content">{post.text}</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
